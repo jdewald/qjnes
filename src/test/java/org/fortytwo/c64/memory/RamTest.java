b105b69f24546b02c568e648b32e3b17e2693e46
Krzysztof Drzazga <krzysztof.drzazga@capgemini.com>
2018-08-02 20:37:48 +0200

    Refining tests

--- src/test/java/org/fortytwo/c64/memory/RamTest.java
+++ src/test/java/org/fortytwo/c64/memory/RamTest.java
@@ -1,19 +1,30 @@
 package org.fortytwo.c64.memory;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class RamTest {
 
-    @Test
-    public void testReadWrite(){
-        RAM ram = new RAM(64 * 1024);
+    private RAM ram;
 
+    @Before
+    public void init() {
+        ram = new RAM(64 * 1024);
+    }
+
+    @Test
+    public void testReadWrite() {
         ram.write(0xd020, 2);
         ram.write(53281, 4);
 
         assertEquals(2, ram.read(0xd020));
         assertEquals(4, ram.read(53281));
+    }
+
+    @Test(expected = Exception.class)
+    public void testWriteToCellOver64Kb() {
+        ram.write(65536, 1);
     }
 }
